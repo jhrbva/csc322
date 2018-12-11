b16f25189cf0ea312f4dc9c5f7e34824c4df7045
Julia Helena <jh@juliahelena.me>
2018-12-11 18:26:01 -0500

    version history method for file class

--- file.py
+++ file.py
@@ -2,15 +2,29 @@ import datetime
 
 class File:
 
+    #class attribute
+    file_visibility = ['private', 'shared', 'public']
+    
     # initializer
-    def __init__(self, file_id, title, author, date, authority, friend_list, history):
+    def __init__(self, file_id, title, text, author, date, visibility, friend_list, history):
         self.file_id = file_id
         self.title = title
+        self.text = text
         self.author = author
         self.date = date
-        self.authority = authority
+        self.visibility = File.file_visibility[visibility]
         self.friend_list = friend_list
         self.history = history
+
+    # move upward or downward in the file history
+    def version_history(self, way):
+        i = self.history
+        if way == 'forward':
+            self.history = i + 1
+        else:
+            self.history = i - 1
+
+        return self.history
 
     #instance method
     def file_information(self):
@@ -22,12 +36,14 @@ class File:
     Created On: {}
     Visibility: {}
     Shared With: {}
-    Version: {}'''.format(self.file_id, self.title, self.author, self.date, self.authority, self.friend_list, self.history)
+    Version: {}'''.format(self.file_id, self.title, self.author, self.date, self.visibility, self.friend_list, self.history)
 
 
 #testing
-my_file = File(123, "My File", "Julia", datetime.date.today(), "private", "Yannis", "0.1")
+#for file visibility : 0 = private, 1 = shared, 2 = public
+my_file = File(123, "My File", "This is the text in my file", "Julia", datetime.date.today(), 1, "Yannis", 1)
 print(my_file.file_information())
+my_file.version_history('forward')
     
 #   question:
 #   how can we prevent date from changing everytime we change something on the file? or do we want the date to change?
