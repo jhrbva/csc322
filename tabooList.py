445498c2de647ef569ba1c03831ad2aa8803b595
Julia Helena <jh@juliahelena.me>
2018-12-11 16:48:39 -0500

    taboo class string scanning method and test files

--- tabooList.py
+++ tabooList.py
@@ -1,48 +1,67 @@
-import fileinput
+import os
 
 class Taboo_List():
 
-    #initializer
-    def __init__(self, file_name, word):
-        self.file_name = file_name
-        self.word = word
+    # initializer (tabooList_file = "tabooList.txt", system_file = any file in the system, will use "testFile.txt" for this)
+    def __init__(self, tabooList_file, taboo_word, system_file):
+        self.tabooList_file = tabooList_file
+        self.taboo_word = taboo_word
+        self.system_file = system_file
 
-    # write method
+    # adding a taboo word to the list method
     def add_word(self):
         """ I add words to the Taboo List """
-        word = input("Enter the taboo word: ")
-        with open(self.file_name, "a+") as f:
-            f.write(word + '\n')
-
+        word = raw_input('Enter the taboo word: ')
+        with open(self.tabooList_file, 'r') as file:
+            filedata = file.read()
+            # THIS CONDITIONAL IS NOT WORKING - IT JUST ADD REPEATED WORDS TO THE FILE
+            if filedata == word :
+                print('This word is already on the list')
+            else :
+                with open(self.tabooList_file, 'a') as file:
+                    file.write(word + '\n')
         print("Your word has been added to the list")
 
-    #delete method
+    
+    # deleting word from taboo list method
     def remove_word(self):
         """ I remove words to the Taboo List """
-        word = input("Enter the taboo word: ")
+        #word = raw_input("Enter the taboo word: ")
         # Read in the file
-        with open(self.file_name, 'r') as file :
+        with open(self.tabooList_file, 'r') as file :
             filedata = file.read()
 
         # Replace the target string
-        filedata = filedata.replace(word, ' ')
+        filedata = filedata.replace(self.taboo_word, ' ')
 
         # Write the file out again
-        with open(self.file_name, 'w') as file:
+        with open(self.tabooList_file, 'w') as file:
             file.write(filedata)
-
         print("The word has been removed")
         
     #decision making
     def add_or_delete(self):
         """ I help decide if the user will add or delete a taboo word from the list """
-        user_action = input("Type A to add the word to the list, type R to remove the word from the list: ")
+        user_action = raw_input("Type A to add the word to the list, type R to remove the word from the list: ")
 
         if user_action == "A":
             self.add_word()
         if user_action == "R":
             self.remove_word()
-       
+
+    # taboo word text scanner
+    def text_scanner(self):
+        """ I help scan files to find taboo words and delete them from text """
+        with open(self.tabooList_file) as file1, open(self.system_file) as file2:
+            for line in set(line.strip() for line in file1).intersection(line.strip() for line in file2):
+                if line: 
+                    print(line)
+                    line = line.replace(line, '**')
+                    
+                    with open(self.system_file, 'w') as file:
+                        file.write(line)
+
 #testing
-tabooWord = Taboo_List("tabooList.txt", "")
+tabooWord = Taboo_List("tabooList.txt", "idiot", "testFile.txt").text_scanner()
+tabooWord = Taboo_List("tabooList.txt", "idiot", "testFile.txt").add_or_delete()
 
