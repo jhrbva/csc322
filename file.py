789cf1c43f06371b0b1d4c248845ade48b784247
Julia Helena <jh@juliahelena.me>
2018-12-11 18:36:07 -0500

    change text method for file class

--- file.py
+++ file.py
@@ -18,13 +18,14 @@ class File:
 
     # move upward or downward in the file history
     def version_history(self, way):
-        i = self.history
         if way == 'forward':
-            self.history = i + 1
+            self.history = self.history + 1
         else:
-            self.history = i - 1
+            self.history = self.history - 1
 
-        return self.history
+    # update text in file
+    def text_update(self, new_text):
+        self.text = new_text
 
     #instance method
     def file_information(self):
@@ -32,18 +33,22 @@ class File:
     File Information:
     ID: {}
     Name: {}
+    Text : {}
     Author: {}
     Created On: {}
     Visibility: {}
     Shared With: {}
-    Version: {}'''.format(self.file_id, self.title, self.author, self.date, self.visibility, self.friend_list, self.history)
+    Version: {}'''.format(self.file_id, self.title, self.text, self.author, self.date, self.visibility, self.friend_list, self.history)
 
 
 #testing
 #for file visibility : 0 = private, 1 = shared, 2 = public
 my_file = File(123, "My File", "This is the text in my file", "Julia", datetime.date.today(), 1, "Yannis", 1)
+print('First version of file:')
 print(my_file.file_information())
 my_file.version_history('forward')
-    
+my_file.text_update('My file is so much better now that I updated the text')
+print('Modified version of file:')
+print(my_file.file_information())
 #   question:
 #   how can we prevent date from changing everytime we change something on the file? or do we want the date to change?
